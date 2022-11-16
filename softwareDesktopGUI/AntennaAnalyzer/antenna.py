1b5eb19da058048d2eba98d8636d4310c9f2b9db
hamza <hmshahid@iastate.edu>
2022-11-16 15:02:10 -0600

    ui change only do not change main.py, added ui changes for look

--- softwareDesktopGUI/AntennaAnalyzer/antenna.py
+++ softwareDesktopGUI/AntennaAnalyzer/antenna.py
@@ -7,7 +7,6 @@
 
 
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QMessageBox, QFileDialog
 
 
 class Ui_AntennaRadiationPatternAnalyzer(object):
@@ -15,9 +14,21 @@ class Ui_AntennaRadiationPatternAnalyzer(object):
         AntennaRadiationPatternAnalyzer.setObjectName("AntennaRadiationPatternAnalyzer")
         AntennaRadiationPatternAnalyzer.resize(1223, 600)
         AntennaRadiationPatternAnalyzer.setStyleSheet("")
-        self.manualRunner = QtWidgets.QRadioButton(AntennaRadiationPatternAnalyzer)
+        self.uploadData = QtWidgets.QPushButton(AntennaRadiationPatternAnalyzer)
+        self.uploadData.setGeometry(QtCore.QRect(330, 510, 121, 41))
+        self.uploadData.setObjectName("uploadData")
+        self.manualRunnerContainer = QtWidgets.QFrame(AntennaRadiationPatternAnalyzer)
+        self.manualRunnerContainer.setGeometry(QtCore.QRect(30, 40, 201, 441))
+        self.manualRunnerContainer.setStyleSheet("background-color:rgb(211,211,211)")
+        self.manualRunnerContainer.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.manualRunnerContainer.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.manualRunnerContainer.setObjectName("manualRunnerContainer")
+        self.headAngleMan = QtWidgets.QLabel(self.manualRunnerContainer)
+        self.headAngleMan.setGeometry(QtCore.QRect(30, 60, 151, 20))
+        self.headAngleMan.setObjectName("headAngleMan")
+        self.manualRunner = QtWidgets.QRadioButton(self.manualRunnerContainer)
         self.manualRunner.setEnabled(True)
-        self.manualRunner.setGeometry(QtCore.QRect(30, 50, 161, 31))
+        self.manualRunner.setGeometry(QtCore.QRect(20, 10, 161, 31))
         font = QtGui.QFont()
         font.setBold(True)
         font.setItalic(False)
@@ -25,46 +36,33 @@ class Ui_AntennaRadiationPatternAnalyzer(object):
         self.manualRunner.setChecked(False)
         self.manualRunner.setAutoExclusive(True)
         self.manualRunner.setObjectName("manualRunner")
-        self.manualRunner.toggled.connect(lambda: self.checkManualRadioButton(self.manualRunner))
-        self.homeDevice = QtWidgets.QPushButton(AntennaRadiationPatternAnalyzer)
-        self.homeDevice.setGeometry(QtCore.QRect(50, 360, 121, 31))
-        self.homeDevice.setStyleSheet(" background-color: Black; color: white }")
+        self.headAngleMan_2 = QtWidgets.QLabel(self.manualRunnerContainer)
+        self.headAngleMan_2.setGeometry(QtCore.QRect(30, 210, 151, 20))
+        self.headAngleMan_2.setObjectName("headAngleMan_2")
+        self.spinBox = QtWidgets.QSpinBox(self.manualRunnerContainer)
+        self.spinBox.setGeometry(QtCore.QRect(30, 250, 141, 22))
+        self.spinBox.setStyleSheet("background-color:rgb(255,255,255)")
+        self.spinBox.setObjectName("spinBox")
+        self.spinBox_2 = QtWidgets.QSpinBox(self.manualRunnerContainer)
+        self.spinBox_2.setGeometry(QtCore.QRect(30, 100, 141, 22))
+        self.spinBox_2.setStyleSheet("background-color:rgb(255,255,255)")
+        self.spinBox_2.setObjectName("spinBox_2")
+        self.homeDevice = QtWidgets.QPushButton(self.manualRunnerContainer)
+        self.homeDevice.setGeometry(QtCore.QRect(30, 350, 141, 41))
+        self.homeDevice.setStyleSheet("QPushButton {background-color: black; color: white;}")
         self.homeDevice.setObjectName("homeDevice")
-        self.uploadData = QtWidgets.QPushButton(AntennaRadiationPatternAnalyzer)
-        self.uploadData.setGeometry(QtCore.QRect(330, 520, 111, 31))
-        self.uploadData.setObjectName("uploadData")
-        self.uploadData.clicked.connect(self.uploadFiles)
 
         self.retranslateUi(AntennaRadiationPatternAnalyzer)
         QtCore.QMetaObject.connectSlotsByName(AntennaRadiationPatternAnalyzer)
 
-    def checkManualRadioButton(self, b):
-        if (b.isChecked()):
-            print("here")
-        else:
-            print("h")
-
-    def uploadFiles(self):
-        fileName = QFileDialog.getOpenFileName(None, 'Open File', "", "Image Files (*.png)")
-        print(fileName)
-        if (fileName[0] != '' and fileName[1] != ''):
-            print("uploaded success")
-            self.successPopup(fileName[0])
-        else:
-            print("failed to upload file try again")
-
-    def successPopup(self, path):
-        msg = QMessageBox()
-        msg.setWindowTitle("Pop Up")
-        msg.setText("Upload Successful from " + path)
-
-        x = msg.exec()
     def retranslateUi(self, AntennaRadiationPatternAnalyzer):
         _translate = QtCore.QCoreApplication.translate
         AntennaRadiationPatternAnalyzer.setWindowTitle(_translate("AntennaRadiationPatternAnalyzer", "Widget"))
-        self.manualRunner.setText(_translate("AntennaRadiationPatternAnalyzer", "Manual Script Runner"))
-        self.homeDevice.setText(_translate("AntennaRadiationPatternAnalyzer", "Home Device"))
         self.uploadData.setText(_translate("AntennaRadiationPatternAnalyzer", "Upload File Data"))
+        self.headAngleMan.setText(_translate("AntennaRadiationPatternAnalyzer", "Angle of Antenna Head?"))
+        self.manualRunner.setText(_translate("AntennaRadiationPatternAnalyzer", "Manual Script Runner"))
+        self.headAngleMan_2.setText(_translate("AntennaRadiationPatternAnalyzer", "Angle of Arc Increment?"))
+        self.homeDevice.setText(_translate("AntennaRadiationPatternAnalyzer", "Home Device"))
 
 
 if __name__ == "__main__":
